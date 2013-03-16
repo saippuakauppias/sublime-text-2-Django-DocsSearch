2a552af5e99ecb361857cc976fb8304152636169
Chase Lee <umchaselee@gmail.com>
2013-03-16 01:24:03 -0300

    Update djangodocssearch.py
    Load docs version from settings into search url

--- djangodocssearch.py
+++ djangodocssearch.py
@@ -6,9 +6,11 @@ import sublime, sublime_plugin
 class DjangoDocsSearchCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+        settings = sublime.load_settings('DjangoDocsSearch.sublime-settings')
+        version = settings.get('version')
         for region in self.view.sel():
             word = self.view.word(region)
             if not word.empty():
-                url = "http://readthedocs.org/docs/django/en/1.3/"\
-                		"search.html?q=%s" % self.view.substr(word)
+                url = "http://readthedocs.org/docs/django/en/%s/"\
+                		"search.html?q=%s" % (version,self.view.substr(word))
                 sublime.active_window().run_command('open_url', {"url": url})
